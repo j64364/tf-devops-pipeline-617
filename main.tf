40bda5670b6a18c8f54dd07598810169ef294563
j64364 <niemijl@gmail.com>
2025-07-17 15:52:08 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 # Root
-
-provider "azurerm" {
-  features {}
-  subscription_id = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc"
-}
-
 provider "azurerm" {
   features {}
   subscription_id = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc" 
