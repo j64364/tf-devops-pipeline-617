59f3f523367d501cdfeb8b1f3bf4da705071bfe2
j64364 <niemijl@gmail.com>
2025-07-17 15:46:52 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "jn-basic-pipeline"
+    storage_account_name = "j64364sa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
